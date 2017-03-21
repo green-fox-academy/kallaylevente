bfd7fc1e9527ccd613a9bb9d40d5fee82773c47e
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 14:00:43 +0100

    finish with the HelloOthers exercise

---
+++ week-02/day-02/workshop07/src/HelloOthers.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        System.out.println("Hello, Andi!");
+        System.out.println("Hello, Balazs!");
+        System.out.println("Hello, Virag!");
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+    }
+}
