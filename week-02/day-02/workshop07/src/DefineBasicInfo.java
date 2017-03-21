b2e19dd93d8c602d6ba2e16a4d12afa7b3ee2608
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 15:18:52 +0100

    Finish the DefineBasicInfo exercise

---
+++ week-02/day-02/workshop07/src/DefineBasicInfo.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Levente" ;
+        int age = 30 ;
+        double heigth = 1.86 ;
+        boolean married = true;
+        System.out.println("My name is: " + name);
+        System.out.println("My age is: " +age);
+        System.out.println("My heigth is: " + heigth);
+        System.out.println("I'am married, and thats the: " + married);
+    }
+}
