a1fed163af36a76f4253350c1af16698e6324e0d
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 14:30:56 +0100

    finishing the TwoNumbers exercise

---
+++ week-02/day-02/workshop07/src/TwoNumbers.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class TwoNumbers {
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 13;
+        double d = ((double)a ) / b;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(d);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
