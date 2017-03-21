959762e3356a893583cf050446ea04f711e44cdc
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 13:45:19 +0100

    Finished the Workshop08

---
+++ week-02/day-02/workshop07/src/Workshop08.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class Workshop08 {
+    public static void main(String[] args) {
+        int h1 = 350;
+        int h2 = 100;
+        // tell if the double of h2 is bigger than h1
+        if ( h1 >  ( 2 * h2 ) )  {
+            System.out.println("The double of h2 is not bigger than h1");
+        }
+        else {
+            System.out.println("The double of h2 is bigger than h1");
+        }
+    }
+}
