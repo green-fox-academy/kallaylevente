1d2dae5ff01280137371b631841976fc08b465f3
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 13:57:38 +0100

    Star to do the real workshop exercises, finished with HelloMe

---
+++ week-02/day-02/workshop07/src/Workshop10.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class Workshop10 {
+    public static void main(String[] args) {
+        int j1 = 10;
+        int j2 = 3;
+        // tell if j1 is between j2 squared and j2 cubed
+
+        System.out.println();
+    }
+}
