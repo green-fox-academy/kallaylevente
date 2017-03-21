1c42304baa8818fe086cf29f00fa87a8fa0de5d7
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 13:53:44 +0100

    finished with the Workshop09

---
+++ week-02/day-02/workshop07/src/Workshop09.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class Workshop09 {
+    public static void main(String[] args) {
+        long i = 1357988018575473L;
+        // tell if it has 11 as a divisor
+        if ((i % 11) == 0) {
+            System.out.println("11 is dividor of the number " + i );
+        }
+        else {
+            System.out.println("11 is not a dividor of the number " + i);
+        }
+    }
+}
