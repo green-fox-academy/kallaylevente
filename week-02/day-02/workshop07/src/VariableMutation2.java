d4e5390b821cb124bb05814c21f92c46ef3b7547
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 17:43:13 +0100

    start to work on VariableMutation2

---
+++ week-02/day-02/workshop07/src/VariableMutation2.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class VariableMutation2 {
+    public static void main(String[] args) {
+        double a = 24;
+        int out = 0;
+        if  ( (a % 2) == 0 ) {
+            out ++ ;
+        }
+        System.out.println(out);
+    }
+}
