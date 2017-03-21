da3f96ba176a43a4ae0cfa529c45b501ee1ddbbe
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 13:37:27 +0100

    Starting the work, and doing workshop07

---
+++ week-02/day-02/idea/src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("ALMA");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+    }
+}
---
+++ week-02/day-02/workshop07/src/Workshop07.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class Workshop07 {
+    public static void main(String[] args) {
+        int g1 = 345;
+        int g2 = 123;
+        // tell if g1 is bigger than g2
+        if (g1 >  g2) {
+            System.out.println("G1 is bigger");
+        }
+        else{
+            System.out.println("G1 is not bigger");
+        }
+
+    }
+}
