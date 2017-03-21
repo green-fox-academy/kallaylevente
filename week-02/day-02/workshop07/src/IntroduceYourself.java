e7c1c262483ff2edea79b1b7e1ac77d29ae4c76e
Kallay Levente <kallaylevente@gmail.com>
2017-03-21 14:13:08 +0100

    finish the IntroduceYourself exercise

---
+++ week-02/day-02/workshop07/src/IntroduceYourself.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Levente on 2017. 03. 21..
+ */
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        String name = "Levente" ;
+        int age = 30 ;
+        double heigth = 1.86 ;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(heigth);
+    }
+}
