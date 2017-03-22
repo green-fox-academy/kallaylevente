fa22b8f90a2287c6c27c6517fc97044fe05bb3c1
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 15:31:22 +0100

    finish the summing of the elements exercise'

---
+++ week-02/day-03/src/sumElements.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class sumElements {
+    public static void main(String[] args) {
+        int[] r = {52, 23, 66, 12};
+        int sum = r[1] + r[2];
+        System.out.println(sum);
+    }
+}
