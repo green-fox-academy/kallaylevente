8c37806d9b5bf5ef2c00f96240e09529819efa66
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 15:27:03 +0100

    finish the comparelength and the previous exercise

---
+++ week-02/day-03/src/compareLength.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class compareLength {
+    public static void main(String[] args) {
+        int[] p1 = {1, 2, 3};
+        int[] p2 = {4, 5, 6, 7};
+        if (p1.length < p2.length) {
+            System.out.println("p2 has more elements");
+        }
+    }
+}
