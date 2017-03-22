a39916567c32bc7f721e399398a1ce4bf50a645d
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 11:29:50 +0100

    start todays exercises

---
+++ week-02/day-03/src/hundredTimes.java
@@ -0,0 +1,13 @@
+import javax.sound.midi.Soundbank;
+
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class hundredTimes {
+    public static void main(String[] args) {
+        String cheat = "I won't cheat on the exam!";
+        for (int i = 0; i < 100 ; i++) {
+            System.out.println(cheat);
+        }
+    }
+}
