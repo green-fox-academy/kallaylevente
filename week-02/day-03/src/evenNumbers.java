4a11b1ac93d6ff86deb9a70783b398e54e7757f9
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 11:32:40 +0100

    finish the even number writer

---
+++ week-02/day-03/src/evenNumbers.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class evenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i < 501 ; i++) {
+            if ((i % 2) == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
