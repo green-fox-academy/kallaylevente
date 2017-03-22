fb128fc96d36e7bbde1918f2a04ecd5c9e894472
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 16:01:53 +0100

    finish the colors exercise

---
+++ week-02/day-03/src/Colors.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class Colors {
+    public static void main(String[] args) {
+        String[][] colors = {{"lime", "forest green", "olive", "pale green" ,"spring green"},
+                            {"orange red", "red", "tomato"},
+                            {"orchid", "violet", "pink", "hot pink"},
+                            };
+        for (int i = 0; i < colors.length ; i++) {
+            for (int j = 0; j < colors[i].length ; j++) {
+                System.out.print(colors[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
