78228f7cb2d22a584b34e9328b6e4ca49117e04f
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 11:50:53 +0100

    finish the star drawer exercise

---
+++ week-02/day-03/src/starDrawer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class starDrawer {
+    public static void main(String[] args) {
+        System.out.println("I read a number and draw a nice triangle");
+        System.out.println("Please give me the number");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 1; i < (number+1) ; i++) {
+            for (int j = 1; j < (i + 1) ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
