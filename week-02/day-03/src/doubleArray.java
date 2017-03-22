d0a347ffcd7240caad41b426a6ed193024c05f0b
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 15:49:44 +0100

    finish the doubleArray exercise

---
+++ week-02/day-03/src/doubleArray.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class doubleArray {
+    public static void main(String[] args) {
+        int[] ag = {3, 4, 5, 6, 7};
+        for (int i = 0; i < ag.length ; i++) {
+            ag[i] = (ag[i] * 2);
+            System.out.println(ag[i]);
+        }
+
+    }
+}
