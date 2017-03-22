dc767ffe2a046098d658425e756baf51c0f4efdc
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 14:21:04 +0100

    finish the doubling exercise

---
+++ week-02/day-03/src/Doubling.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class Doubling {
+    public static void main(String[] args) {
+        int ak = 123;
+        System.out.println(doubling(ak));
+    }
+
+    public static int doubling(int ak){
+        ak = 2 * ak;
+        return ak;
+    }
+}
