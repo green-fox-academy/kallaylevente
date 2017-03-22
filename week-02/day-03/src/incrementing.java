9a615d92da1c2c43c558d689133fca342d4ef8b0
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 15:38:04 +0100

    finish the increment exercise

---
+++ week-02/day-03/src/incrementing.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class incrementing {
+    public static void main(String[] args) {
+        int[] t = {1, 2, 3, 4, 5};
+        //t[2] = t[2] + 1 ;
+        System.out.println(t[2] +1);
+    }
+}
