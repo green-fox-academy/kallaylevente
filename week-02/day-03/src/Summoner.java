df0b873a90494301d47fd12c15b095c0ec1c639d
Kallay Levente <kallaylevente@gmail.com>
2017-03-22 14:52:32 +0100

    finish the sum exercise

---
+++ week-02/day-03/src/Summoner.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Levente on 2017. 03. 22..
+ */
+public class Summoner {
+    public static void main(String[] args) {
+        int until = 5;
+        System.out.println(summoner(until));
+    }
+    public static int summoner(int until) {
+        int sum = 0;
+        for (int i = 1; i < until+1 ; i++) {
+           sum = sum + i;
+
+        }
+        return sum;
+    }
+}
