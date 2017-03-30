0f655727b0d54827f17d13e7ad1c3ad79bc69f27
Kallay Levente <kallaylevente@gmail.com>
2017-03-30 23:34:44 +0200

    finish the recursive drawing

---
+++ week-03/day-04/src/Hexagon.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Hexagon {
+  final static int SIDES = 6;
+  final static int SIZE = 400;
+  final static int NUMBER = 4;
+  public static void mainDraw(Graphics graphics) {
+    int x = 200;
+    int y = 100;
+    int wX = 400;
+    int hY = 344;
+    paint(graphics,x,y,NUMBER,wX,hY);
+  }
+
+  public static void paint(Graphics graphics, int x, int y,int n,int wX, int hY ) {
+    int[] xPoints = {x,x+wX/2,x+wX/4*3,x+wX/2,x,x-wX/4};
+    int[] yPoints = {y,y,y+hY/2,y+hY,y+hY,y+hY/2};
+    graphics.drawPolygon(xPoints,yPoints,SIDES);
+    if (n == 0) {
+      return;
+    } else {
+      paint(graphics,x,y,n-1,wX/2,hY/2);
+      paint(graphics,x,y+hY/2,n-1,wX/2,hY/2);
+      paint(graphics,x+wX/8*3,y+hY/4,n-1,wX/2,hY/2);
+
+
+
+    }
+  }
+
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(600, 600));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+
+    }
+  }
+
+}
---
+++ week-03/day-04/src/YellowBoxes.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class YellowBoxes {
+
+  public static void mainDraw(Graphics graphics) {
+    int n = 5;
+    int x = 0;
+    int y = 0;
+    int size = 486;
+    paint(graphics, n,x,y,size);
+  }
+
+  public static void paint(Graphics graphics, int n,int x, int y,int size) {
+    graphics.drawRect(x,y,size,size);
+    if (n > 0)  {
+      paint(graphics,n-1,x+(size/3), y,(size/3) );
+      paint(graphics,n-1,x, y+size/3,(size/3) );
+      paint(graphics,n-1,x+(size/3), y+size/3*2,(size/3) );
+      paint(graphics,n-1,x+(size/3*2), y+size/3,(size/3) );
+
+    }
+  }
+
+
+  //    Don't touch the code below
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(300, 300));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      this.setBackground(Color.yellow);
+      mainDraw(graphics);
+
+
+    }
+  }
+
+}
