f58ed69d9847af1976904e9a8298a5dd2c32e0b6
Kallay Levente <kallaylevente@gmail.com>
2017-04-04 17:10:50 +0200

    fix the comments problem

--- week-04/day-02/src/Flower.java
+++ week-04/day-02/src/Flower.java
@@ -8,4 +8,13 @@ public class Flower extends Plants {
   public void watering(double wateramount) {
     this.waterlevel = this.waterlevel + (wateramount * 0.75);
   }
+
+  @Override
+  public void info() {
+    if (isNeed()) {
+      System.out.println("The " + color + " Flower needs water");
+    } else {
+      System.out.println("The " + color + " Flower dont need water");
+    }
+  }
 }
--- week-04/day-02/src/Tree.java
+++ week-04/day-02/src/Tree.java
@@ -8,4 +8,12 @@ public class Tree extends Plants{
   public void watering(double wateramount) {
     this.waterlevel = this.waterlevel + (wateramount * 0.40);
   }
+
+  public void info() {
+    if (isNeed()) {
+      System.out.println("The " + color + " Tree need water");
+    } else {
+      System.out.println("The " + color + " Tree dont need water");
+    }
+  }
 }
