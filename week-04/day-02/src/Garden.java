cd51c05e92beb9a9e37ee7c6096920bb89736af8
Kallay Levente <kallaylevente@gmail.com>
2017-04-04 16:25:38 +0200

    add counter thats count how many thirsty plants there are

--- week-04/day-02/src/Garden.java
+++ week-04/day-02/src/Garden.java
@@ -3,12 +3,30 @@ import java.util.List;
 
 public class Garden {
   public static void main(String[] args) {
+      List<Plants> gardenPlants = new ArrayList<>();
       Plants testPlant = new Plants(12,"black");
       testPlant.waterLimit = 11;
       testPlant.info();
 
+      Flower testFlower = new Flower(4,"red");
+      Tree testTree = new Tree(11,"brown");
+      gardenPlants.add(testFlower);
+      gardenPlants.add(testTree);
+
+      
 
 
+
+    }
+
+    public static int counter(List<Plants> plantList) {
+    int counter = 0;
+      for (int i = 0; i < plantList.size(); i++) {
+        if (plantList.get(i).isNeed()) {
+          counter++ ;
+        }
+      }
+      return counter;
     }
 
 }
