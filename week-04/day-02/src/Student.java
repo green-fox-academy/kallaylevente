5799d459763fc45ea007daffb33fd8aea2251f8a
Kallay Levente <kallaylevente@gmail.com>
2017-04-04 12:14:16 +0200

    finish the student class with the constructors

--- week-04/day-02/src/Student.java
+++ week-04/day-02/src/Student.java
@@ -12,4 +12,20 @@ public class Student extends Person {
   public void introduce() {
     System.out.println("Hi, I'm " + this.getName() + ", a " + this.getAge() + " year old " +  this.getGender() + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
   }
+
+  public void skipDays(int numberOfDays) {
+    this.skippedDays +=numberOfDays;
+  }
+
+  Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  public Student() {
+    super();
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
 }
