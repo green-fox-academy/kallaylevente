8a99fc23a0a970475cea4cf35fa1239ab2427480
Kallay Levente <kallaylevente@gmail.com>
2017-04-05 17:35:41 +0200

    start the cow game

--- week-04/day-03/src/CowsAndBulls.java
+++ week-04/day-03/src/CowsAndBulls.java
@@ -1,17 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class CowsAndBulls {
 
   public static void main(String[] args) {
-
-    int num = 1000 + (int) (Math.random() * 900);
-    System.out.println(num);
+    String state = new String();
+    int counter = 0;
+    int num = 1000 + (int) (Math.random() * 9000);
     ArrayList<Integer> numList = new ArrayList<>();
-
     for (int i = 0; i < 4; i++) {
       numList.add(num % 10);
       num = num / 10;
     }
+    ArrayList<Integer> guessList = new ArrayList<>();
+    System.out.println("Please give me 4 number");
+    Scanner scanner = new Scanner(System.in);
+    int guess = scanner.nextInt();
+    for (int i = 0; i < 4; i++) {
+      guessList.add(guess % 10);
+      guess = guess / 10;
+    }
+
+    for (int i = 0; i < 4; i++) {
+      System.out.println(guessList.get(i));
+
+    }
+
+
+
+
+
 
 
 
