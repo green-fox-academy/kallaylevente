94347bd824ec252c938452eec5c731bfc7425bd2
Kallay Levente <kallaylevente@gmail.com>
2017-04-11 09:57:53 +0200

    add some more directories

---
+++ week-04/day-03/src/Consol.java
@@ -0,0 +1,9 @@
+public class Consol {
+  private final static String FILE_NAME = "data.csv";
+
+  public static void main(String[] args) {
+
+    System.out.println("I am " + args[0]);
+
+  }
+}
---
+++ week-04/day-04/src/Todo.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Levente on 2017. 04. 06..
+ */
+public class Todo {
+}
