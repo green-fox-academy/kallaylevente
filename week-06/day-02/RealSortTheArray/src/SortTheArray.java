b6133efd07db8228ad0e2fc5527330dbaf51d86e
Kallay Levente <kallaylevente@gmail.com>
2017-04-19 15:16:54 +0200

    finish the sort the array exercise

---
+++ week-06/day-02/RealSortTheArray/src/SortTheArray.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class SortTheArray {
+  public static void main(String[] args) {
+    Integer[] intArray = { 1, 2, 3, 4, 5 };
+    Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+
+    listSorter(intArray);
+    listSorter(doubleArray);
+
+  }
+
+  public static <T extends Number> void listSorter(T[] inputList) {
+    double[] answerArray = new double[inputList.length];
+
+    for (int i = 0; i < inputList.length; i++) {
+      answerArray[i] = inputList[(inputList.length - 1 - i)].doubleValue();
+      System.out.println(answerArray[i]);
+    }
+
+  }
+}
