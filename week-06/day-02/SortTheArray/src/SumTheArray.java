353356074b17803a8ee8481f56dd819c0a5438f8
Kallay Levente <kallaylevente@gmail.com>
2017-04-19 14:12:53 +0200

    use ternary operator in exception to make it fancy

--- week-06/day-02/SortTheArray/src/SumTheArray.java
+++ week-06/day-02/SortTheArray/src/SumTheArray.java
@@ -6,7 +6,7 @@ public class SumTheArray {
     Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
 
     System.out.println(arraySummer(intArray,5));
-    System.out.println(arraySummer(doubleArray,5));
+    System.out.println(arraySummer(doubleArray,5) == 0 ? "Given number is bigger than length of the array" : arraySummer(doubleArray,5) );
   }
 
   public static <T extends Number>  double arraySummer( T[] inputArray, int length){
@@ -16,7 +16,7 @@ public class SumTheArray {
         sum = sum + inputArray[i].doubleValue();
       }
     }catch (IndexOutOfBoundsException e) {
-      System.out.println("Given number is bigger than the length of the given array");
+      sum = 0;
 
     }
     return sum;
