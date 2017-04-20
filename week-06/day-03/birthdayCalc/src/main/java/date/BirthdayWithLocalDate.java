718c26b6f082e7c17dc3e8b8d7da1f6363d34816
Kallay Levente <kallaylevente@gmail.com>
2017-04-20 14:39:19 +0200

    finish the birthday exercise

---
+++ week-06/day-03/birthdayCalc/src/main/java/date/BirthdayWithLocalDate.java
@@ -0,0 +1,88 @@
+package date;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BirthdayWithLocalDate implements BirthdayCalculator<LocalDate> {
+
+  @Override
+  public LocalDate parseDate(String str) {
+    // TODO - return with the parsed date; format is: yyyy-MM-dd
+    LocalDate date = LocalDate.parse(str);
+    return date;
+  }
+
+  @Override
+  public String printMonthAndDay(LocalDate date) {
+    // TODO - return the date formatted: month & day (MM. dd.)
+    return date.format(DateTimeFormatter.ofPattern("MM. dd."));
+  }
+
+  @Override
+  public boolean isAnniversaryToday(LocalDate date) {
+    // TODO - return with true if today is the same month+day as date
+    if (date.equals(null)) {
+      return false;
+    }
+    LocalDate todayDate = LocalDate.now();
+    return (todayDate.equals(date));
+  }
+
+  @Override
+  public int calculateAgeInYears(LocalDate birthday) {
+    // TODO - return how many years age the input date 'birthday' was
+    return LocalDate.now().getYear() - birthday.getYear();
+  }
+
+  @Override
+  public int calculateDaysToNextAnniversary(LocalDate date) {
+    // TODO - the number of days remaining to the next anniversary of 'date' (e.g. if tomorrow, return 1)
+    LocalDate now = LocalDate.now();
+    int expected;
+    if (now.getDayOfYear() == date.getDayOfYear()) {
+      expected = 0;
+    } else if (now.getDayOfYear() > date.getDayOfYear()) {
+      return LocalDate.of(now.getYear(), 12, 31).getDayOfYear() - now.getDayOfYear() + date.getDayOfYear();
+    } else {
+      return date.getDayOfYear() - now.getDayOfYear();
+    }
+    return expected;
+}
+
+  public static void main(String[] args) {
+    new BirthdayWithLocalDate().run();
+  }
+
+  private void run() {
+
+    print("Birthday with java.util.Date.");
+    String birthdayStr = readInput("What day were you born (yyyy-mm-dd)?");
+
+    LocalDate birthdayDate = parseDate(birthdayStr);
+    print("Your birthday: " + printMonthAndDay(birthdayDate));
+
+    if (isAnniversaryToday(birthdayDate)) {
+      int ageInYears = calculateAgeInYears(birthdayDate);
+      print("Congratulations! Today is your " + ageInYears + "th birthday!");
+    } else {
+      int daysLeft = calculateDaysToNextAnniversary(birthdayDate);
+      print("You have to wait only " + daysLeft + " days for your next birthday.");
+    }
+  }
+
+  private void print(String line) {
+    System.out.println(line);
+  }
+
+  private String readInput(String message) {
+    System.out.print(message + ": ");
+    return input.nextLine();
+  }
+
+  private final Scanner input = new Scanner(System.in, "UTF-8");
+}
