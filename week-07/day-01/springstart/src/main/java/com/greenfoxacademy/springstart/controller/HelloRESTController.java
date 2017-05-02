a6261328ffd39f2a0d322e646ce9521fc61ed054
Kallay Levente <kallaylevente@gmail.com>
2017-05-02 12:05:14 +0200

    add counter feature

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.springstart.controller;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
+  AtomicLong counter = new AtomicLong();
+
   @RequestMapping("/greeting")
   public Greeting greeting(@RequestParam(value = "name" ) String name) {
-    String greet = "Hello ;" + name;
-    return new Greeting(1, name);
+    return new Greeting(counter.addAndGet(1), ("Hello ;" + name));
   }
 
 }
