7e2de13b81e49c3b8fca07a4bc387614c5f08055
Kallay Levente <kallaylevente@gmail.com>
2017-05-02 11:53:19 +0200

    add greet with your name

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controller/HelloRESTController.java
@@ -1,14 +1,16 @@
 package com.greenfoxacademy.springstart.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
   @RequestMapping("/greeting")
-  public Greeting greeting() {
-    return new Greeting(1, "Hello World!");
+  public Greeting greeting(@RequestParam(value = "name" ) String name) {
+    String greet = "Hello ;" + name;
+    return new Greeting(1, name);
   }
 
 }
