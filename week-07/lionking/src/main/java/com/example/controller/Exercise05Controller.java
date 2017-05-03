fc0dc451ccea46164948fb0908d2d27cf18143c9
Kallay Levente <kallaylevente@gmail.com>
2017-05-03 13:38:30 +0200

    finish the 5th exercise with the list

---
+++ week-07/lionking/src/main/java/com/example/controller/Exercise05Controller.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import com.example.modell.BankAccount;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Exercise05Controller {
+
+  List<BankAccount> bankAccountList;
+  @RequestMapping("/exercise05")
+  public String listOfAccounts(Model model) {
+    bankAccountList = new ArrayList<>();
+    fillBankAccountList(bankAccountList);
+    model.addAttribute("accountList", bankAccountList);
+    return "index5";
+
+  }
+
+  public void fillBankAccountList(List<BankAccount> accountList) {
+    accountList.add(new BankAccount("Pumba", "1000", "warthog"));
+    accountList.add(new BankAccount("Timon", "500", "meerkat"));
+    accountList.add(new BankAccount("Fred", "2500", "antilop"));
+    accountList.add(new BankAccount("Sterve", "750", "coyote"));
+  }
+
+}
