4eb6383ffcf5899d01fb19ea1d7d5189e85718b5
Kallay Levente <kallaylevente@gmail.com>
2017-07-10 09:40:17 +0200

    feature: solve the problem

--- week16/src/OnlyFirstArrayNumbers.java
+++ week16/src/OnlyFirstArrayNumbers.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class OnlyFirstArrayNumbers {
 
+  public static void main(String[] args) {
+    List<Integer> listA =listA = fillListAWithNumbers();
+    List<Integer> listB = listB = fillListBWithNumbers();
+    for (Integer temp : returnFirstListsIntegerOnly(listA, listB)) {
+      System.out.println(temp);
+    }
+  }
+
+  private static List<Integer> returnFirstListsIntegerOnly(List<Integer> listA, List<Integer> listB) {
+    List<Integer> returnList = new ArrayList<>();
+    for (int i = 0; i < listA.size() ; i++) {
+      if (!listB.contains(listA.get(i))) {
+        returnList.add(listA.get(i));
+      }
+    }
+    return returnList;
+  }
+
+  private static List<Integer> fillListAWithNumbers() {
+    List<Integer> listA = new ArrayList<>();
+    listA.add(1);
+    listA.add(2);
+    listA.add(3);
+    listA.add(4);
+    return listA;
+  }
+
+  private static List<Integer> fillListBWithNumbers() {
+    List<Integer> listB = new ArrayList<>();
+    listB.add(3);
+    listB.add(4);
+    listB.add(5);
+    listB.add(6);
+    return listB;
+  }
 }
 
 
