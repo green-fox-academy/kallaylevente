3488969066251e127fddc02bc38c9a03b04d067d
Kallay Levente <kallaylevente@gmail.com>
2017-07-10 09:40:55 +0200

    remove comment

--- week16/src/OnlyFirstArrayNumbers.java
+++ week16/src/OnlyFirstArrayNumbers.java
@@ -40,6 +40,3 @@ public class OnlyFirstArrayNumbers {
   }
 }
 
-
-//Create a function that takes two list of
-// numbers and returns a new list that only consists those numbers that are in the first array but not in the second
